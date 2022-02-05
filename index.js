423e9c52da220cb83b7d99b6e8ecd8282abb0d6f
Sanganu <kppkmsanu@gmail.com>
2022-02-05 12:22:01 -0700

    Adding Employee.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// class Node{
+   
+//     constructor(data,left=null,right=null){
+//         this.data = data;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+
+// class BST{
+    
+// }
